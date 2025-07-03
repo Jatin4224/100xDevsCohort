eef79caab9b704f860ffb7602ea9f7e2249b9d64
Jatin4224 <jaikumarsharma94130@gmail.com>
2025-07-03 10:27:40 +0530

    Binary Search

---
+++ nextjs/NEXTJSDEPTH/RandomJoke/config/db.tsx
@@ -0,0 +1,9 @@
+import mysql from "mysql2/promise";
+
+export mysql.createPool({
+    const db = mysql.createPool({
+        host:"localhost",
+        user:"root",
+        password:"@Jatin4224"
+    })
+});
