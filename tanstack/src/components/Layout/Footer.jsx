58a880df440b364eae7a5d915416db79f9610f4b
Jatin4224 <jaikumarsharma94130@gmail.com>
2025-07-04 08:19:58 +0530

    UI updated

--- tanstack/src/components/Layout/Footer.jsx
+++ tanstack/src/components/Layout/Footer.jsx
@@ -1,7 +1,12 @@
 import React from "react";
+import "./Header.css"; // Link to dark theme styles
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <footer className="footer">
+      <p>© {new Date().getFullYear()} TANSTACK QUERY. All rights reserved.</p>
+    </footer>
+  );
 };
 
 export default Footer;
--- tanstack/src/components/Layout/Header.jsx
+++ tanstack/src/components/Layout/Header.jsx
@@ -1,7 +1,21 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
+import "./Header.css"; // Link to dark theme styles
 
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <header className="navbar">
+      <div className="navbar-brand">Tanstack</div>
+      <nav className="nav-links">
+        <NavLink to="/rq" className="nav-link" activeclassname="active">
+          FetchRQ
+        </NavLink>
+        <NavLink to="/trad" className="nav-link" activeclassname="active">
+          FetchOld
+        </NavLink>
+      </nav>
+    </header>
+  );
 };
 
 export default Header;
