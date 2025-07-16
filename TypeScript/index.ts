606d9501885452bd4b237f3d0227e495002c528b
Jatin4224 <jaikumarsharma94130@gmail.com>
2025-07-16 16:06:26 +0530

    unions

--- TypeScript/index.ts
+++ TypeScript/index.ts
@@ -1,5 +1,56 @@
-//  let  myVar: type = value
+// let coffe: boolean = "black";
+// console.log(coffe);
 
-let chaiFlavour: string = "masala chai";
-chaiFlavour = 2231;
-console.log(chaiFlavour);
+// // TypeScript will give an error
+// // if I try to assign a number or string to `coffee`.
+// // For example:
+// // coffee = 100;      ❌ Error
+// // coffee = "black";  ❌ Error
+// // Because the variable `coffee` is strictly typed as a boolean,
+// // it can only accept `true` or `false` as values.
+
+// //Create a variable of type string and stir in your favorite string method (e.g., .toUpperCase() or .includes()).
+
+// // 1. Create a variable of type string and stir in your favorite string method (e.g., .toUpperCase() or .includes()).
+// let chaiFlavour: string = "masala chai";
+// chaiFlavour = chaiFlavour.toUpperCase();
+// console.log(chaiFlavour);
+
+// //2.
+// let sugarCubes: number = 2;
+// sugarCubes = sugarCubes + 3;
+// console.log(sugarCubes);
+// //3.
+// let isChaiHot: boolean = true;
+// let isChaiReady: boolean = false;
+
+// let canServeChai: boolean = isChaiHot && isChaiReady;
+// console.log(canServeChai);
+
+// chaiFlavour = 42;
+// sugarCubes = "five";
+// isChaiHot = "yes";
+
+// let Drink = "chai";
+
+// let cups = Math.random() > 0.5 ? 10 : 5;
+
+// let firstName = "Hitesh";
+
+// firstName.length();
+
+// let let, Name;
+
+// console.Hiteshdotcom("Work hard, learn fast. If he can do it, you can too.");
+
+let channelName = "chai";
+channelName = true;
+
+let theme: any = "chai theme";
+theme: 20;
+theme: true;
+theme();
+theme.toUpperCase();
+console.log(theme);
+
+let subcribers: number | string = 10;
