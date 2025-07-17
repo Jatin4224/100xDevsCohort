cb4095779501e40f6b16dfd461b399e5ff58d444
Jatin4224 <jaikumarsharma94130@gmail.com>
2025-07-17 17:03:29 +0530

    function ts

--- TypeScript/index.ts
+++ TypeScript/index.ts
@@ -43,14 +43,65 @@
 
 // console.Hiteshdotcom("Work hard, learn fast. If he can do it, you can too.");
 
-let channelName = "chai";
-channelName = true;
+// let channelName = "chai";
+// channelName = true;
 
-let theme: any = "chai theme";
-theme: 20;
-theme: true;
-theme();
-theme.toUpperCase();
-console.log(theme);
+// let theme: any = "chai theme";
+// theme: 20;
+// theme: true;
+// theme();
+// theme.toUpperCase();
+// console.log(theme);
 
-let subcribers: number | string = 10;
+// let subcribers: number | string = 10;
+// subcribers = 100000;
+// subcribers = "1million";
+
+// let requestStatus: "pending" | "success" | "error" = "pending";
+// requestStatus = "success";
+// requestStatus = "error";
+// requestStatus = "done";
+
+// const orders = ["12", "20", "28", "42"];
+// let currentOrder: string | undefined;
+
+// for (let order of orders) {
+//   if (order === "28") {
+//     currentOrder = order;
+//     break;
+//   }
+// }
+// console.log(currentOrder);
+
+// ## 🧠 Challenge
+
+//Create a variable `chaiOrder` of type `'ordered' | 'brewing' | 'served'` and assign it the value `'ordered'`. Then,
+//try assigning it the values `'brewing'` and `'served'`.
+
+//Create a variable `chaiDiscount` of type `number | string` and assign it the value `30`. Then, try assigning it the value `'30%'`.
+
+let chaiDiscount: number | string = 20;
+chaiDiscount = "20%";
+//chaiDiscount = true //typescript😡❌
+
+let chaiOrder: "ordered" | "brewing" | "served" = "ordered";
+chaiOrder: "served";
+chaiOrder: "brewing";
+
+function incomingCall(num: number) {
+  return num + 1;
+}
+
+const callsReceived = incomingCall(2);
+console.log(callsReceived);
+
+const outgoingCall = (a: number, b: number) => a * b;
+const callsDialed = outgoingCall(2, 1, 3);
+console.log(callsDialed);
+
+function namaste(person: string = "Hitesh") {
+  return `Namaste ${person}`;
+}
+
+const res = namaste();
+console.log(print);
