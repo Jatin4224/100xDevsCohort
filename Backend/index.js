0cfec3fd7acf6c3f7f2cf4d1c524ffa57be0e0cc
Jatin4224 <jaikumarsharma94130@gmail.com>
2025-10-13 12:52:28 +0530

    routing and req handlers

--- Backend/index.js
+++ Backend/index.js
@@ -1,6 +1,4 @@
 const express = require("express");
 const app = express();
 // todo write a get request show hello world on local host
-app.listen(3000, () => {
-  console.log("lawanaya is hot");
-});
+app.listen(3000, () => {});
---
+++ DevTinder/src/app.js
@@ -0,0 +1,29 @@
+const express = require("express");
+
+const app = express();
+
+app.use("/user", (req, res) => {
+  res.send("HAHAHAHAHAHA");
+});
+
+//this will only handle GET call to /user
+app.get("/user", (req, res) => {
+  res.send({ firstName: "Akshay", lastName: "Saini" });
+});
+
+app.post("/user", (req, res) => {
+  console.log("save data to the db");
+  res.send("data successfully saved to the db");
+});
+
+app.delete("/user", (req, res) => {
+  res.send("deleted successfully!!");
+});
+//this will match all the HTTP method API calls to /test
+app.use("/test", (req, res) => {
+  res.send("Hello from the server!!");
+});
+
+app.listen(7777, () => {
+  console.log("Server is successfully listening on port 7777");
+});
